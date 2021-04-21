0ba07af25007bbb694eb88d84a931c87c17b916e
Ryan Fritz <ryanfritzc@gmail.com>
2021-04-21 16:39:17 -0400

    Completed all videos up to 15

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     /*let name = 'ryan';                                Video 8
@@ -23,16 +24,30 @@ const Home = () => {
         {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'ryan', id: 3},
     ]);
 
+    const [name, setName] = useState('ryan');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('use effect ran')
+        console.log(name)
+    }, [name]);
+
     return ( 
         <div className="home">
-            {blogs.map((blog)=> (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('fritz')}>Change Name</button>
+            <p>{name}</p>
         </div>
-        /*<div className="home">                        Video 8
+        /*<div className="home">                                                                    Video 12
+            <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog)=>blog.author === 'ryan')} title="Ryan's Blogs"/>       |
+        </div>
+
+        <div className="home">                        Video 8
             <h2>Homepage</h2>
             <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
