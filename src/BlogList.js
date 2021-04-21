9962272e585c0ca66f55aee5df1bd85b54206c06
Ryan Fritz <ryanfritzc@gmail.com>
2021-04-21 18:51:40 -0400

    Completed all videos through video 20

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,5 @@
-const BlogList = ({blogs,title, handleDelete}) => {
+const BlogList = ({blogs,title}) => {
+//const BlogList = ({blogs,title, handleDelete}) => {       Video 17
     //const BlogList = (props) => {                     Video 11
     //const blogs = props.blogs;
     //const title = props.title;                            |
@@ -10,11 +11,23 @@ const BlogList = ({blogs,title, handleDelete}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
     );
+    /*return (                                                                          Video 17
+        <div className = "blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog)=> (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+                </div>
+            ))}
+        </div>                                                                              |
+    );*/    
 }
  
 export default BlogList;
+//npx json-server --watch data/db.json --port 8000
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,19 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    /*let name = 'ryan';                                Video 8
+
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+    return ( 
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+        </div>
+    /*const [isPending, setIsPending]= useState(true);                  Video 20
+    const [error, setError] =useState(null);                          Video 20
+    let name = 'ryan';                                Video 8
     const [name, setName] = useState('ryan');           
     const [age, setAge] = useState(22);                 
 
@@ -15,48 +26,71 @@ const Home = () => {
     }
 
     const handleClick2 = (name) => {              
-        console.log('hello ' + name);                       |
-    }*/ 
+        console.log('hello ' + name);                       
+    }                                                       |
 
-    const [blogs, setBlogs] = useState([
+    //const [blogs, setBlogs] = useState(null);                 Video 20
+    /*const [blogs, setBlogs] = useState([                                                  Video 17
         {title: 'My new website', body: 'lorem ipsum...', author: 'ryan', id: 1},
         {title: 'Welcome party', body: 'lorem ipsum...', author: 'fritz', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'ryan', id: 3},
-    ]);
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'ryan', id: 3},             
+    ]);                                                                                         |
 
-    const [name, setName] = useState('ryan');
+    //const [name, setName] = useState('ryan');             Video 15
 
-    const handleDelete = (id) => {
+    /*const handleDelete = (id) => {                                                Video 17
         const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+        setBlogs(newBlogs);                                                             
+    }                                                                                   |
 
-    useEffect(() => {
+    /*useEffect(() => {                                                            Video 20
+        setTimeout(() => {                                                        
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err=> {
+                setIsPending(false);
+                setError(err.message);
+            })                                                                          
+        }, 1000) 
+    },  []);                                                                                |
+    useEffect(() => {                                                                   Video 15
         console.log('use effect ran')
-        console.log(name)
-    }, [name]);
+        console.log(name)                                                                    |
+    }, [name]); */
 
-    return ( 
-        <div className="home">
+        /*<div className="home">                                                                    Video 17
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
+        </div>                                                                                          |
+        <div className="home">                                                                Video 15
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
             <button onClick={() => setName('fritz')}>Change Name</button>
-            <p>{name}</p>
-        </div>
-        /*<div className="home">                                                                    Video 12
+            <p>{name}</p>                                                                           
+        </div>                                                                                    |
+        <div className="home">                                                                    Video 12
             <BlogList blogs={blogs} title="All Blogs!"/>
-            <BlogList blogs={blogs.filter((blog)=>blog.author === 'ryan')} title="Ryan's Blogs"/>       |
-        </div>
+            <BlogList blogs={blogs.filter((blog)=>blog.author === 'ryan')} title="Ryan's Blogs"/>       
+        </div>                                                                                          |
 
         <div className="home">                        Video 8
             <h2>Homepage</h2>
             <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
         </div>                                              |
-        <div className="home">                         Video 7
+        <div className="home">                                                           Video 7
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>            
-            <button onClick={() => handleClick2('fritz')}>Click me 2</button> 
-        </div> */
+            <button onClick={() => handleClick2('fritz')}>Click me 2</button>               
+        </div> */                                   
      );
 }
  
